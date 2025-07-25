6bd6ebd5ab31505ffe23ef622b3476a6f6dec302
LeraLera6 <tinkmink.tink6@gmail.com>
2025-07-25 12:58:14 +0300

    Update start_reporting.py

--- start_reporting.py
+++ start_reporting.py
@@ -1,6 +1,6 @@
+import threading
 from reporting_task import hourly_report_loop
 
 def start_reporting_thread():
-    import threading
-    thread = threading.Thread(target=hourly_report_loop, daemon=True)
-    thread.start()
+    t = threading.Thread(target=hourly_report_loop, daemon=True)
+    t.start()
